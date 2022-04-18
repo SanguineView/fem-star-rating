0176583fcdffb2fe851733a21844f5da55209c6c
Ryan McGuire <ryan.mcguire.va@gmail.com>
2022-04-18 14:09:34 -0400

    Desktop Voting

---
+++ script.js
@@ -0,0 +1,24 @@
+const ratingBtns = document.querySelectorAll('.ratingSelect')
+
+function ratingSelection(event) {
+    let rating = 0
+    const ratingContainer = document.getElementById('starsContainer')
+    ratingContainer.innerHTML = ''
+
+    rating = event.target.innerText
+    
+
+    for (let i = 1; i <= rating; i++) {
+        starEl = document.createElement('img')
+        starEl.src = '/images/icon-star.svg'
+        starEl.className = 'ratingSelect'
+        ratingContainer.append(starEl)
+    }
+    
+    
+
+}
+
+ratingBtns.forEach((button) => {
+    button.addEventListener('click', ratingSelection)
+})
