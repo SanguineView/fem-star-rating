54caa39dc7c2ac986f993f8a6da9de2c1eb556e0
Ryan McGuire <ryan.mcguire.va@gmail.com>
2022-05-09 19:35:18 -0400

    readme update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// you are now declared
 const ratingBtns = document.querySelectorAll('.ratingSelect')
 const submitRatingBtn = document.getElementById('submit')
 const ratingContainer = document.getElementById('starsContainer')
@@ -6,6 +7,7 @@ const messageWindow = document.getElementById('messageWindow')
 
 let rating = 0
 
+// so...how'd we do on a scale of 1 - 5?
 function ratingSelection(event) {
     ratingContainer.innerHTML = ''
     ratingBtns.forEach((item) => {
@@ -24,16 +26,17 @@ function ratingSelection(event) {
     return rating
 }
 
+// thank you, come again
 function ratingSubmit() {
     // move rating window
     ratingWindow.classList.add('hide')
     // populate message in message window
-    document.getElementById('choiceMessage').innerText= `You selected ${rating} out of 5.`
+    document.getElementById('choiceMessage').innerText = `You selected ${rating} out of 5.`
     // move message window
     messageWindow.classList.add('show')
-
 }
 
+// heads up for anything pushed
 ratingBtns.forEach((button) => {
     button.addEventListener('click', ratingSelection)
 })
